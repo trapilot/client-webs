d9a996dc070e29b9768452e97765777b1cc5e733
Nam Dinh <namdwb@gmail.com>
2026-05-24 16:17:49 +0700

    feat: apply meta image

---
+++ 1.hqt_elevator/sql/migrations/0002_page_meta_card_page_meta_image.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `cms_engine_page`
+ADD COLUMN `meta_card` VARCHAR(255) NULL AFTER `meta_keywords`;
+
+ALTER TABLE `cms_engine_page`
+ADD COLUMN `meta_image` VARCHAR(255) NULL AFTER `meta_card`;
