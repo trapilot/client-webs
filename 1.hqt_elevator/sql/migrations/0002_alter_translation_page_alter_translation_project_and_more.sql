3871c58f0da4fd9ec43d77a334fdfcf1bc691ae5
Nam Dinh <namdwb@gmail.com>
2026-05-25 22:11:18 +0700

    fix: db

--- 1.hqt_elevator/sql/migrations/0002_alter_translation_page_alter_translation_project_and_more.sql
+++ 1.hqt_elevator/sql/migrations/0002_alter_translation_page_alter_translation_project_and_more.sql
@@ -1,6 +1,6 @@
 -- Create Banner table
 CREATE TABLE `cms_engine_banner` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
 
     `type` INT NOT NULL,
 
@@ -52,7 +52,25 @@ CREATE TABLE `cms_engine_banner` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
+
 -- Add customer_address field to testimonial table
 ALTER TABLE `cms_engine_testimonial`
 ADD COLUMN `customer_address` VARCHAR(200) NULL
 AFTER `customer_image`;
+
+
+-- Indexes
+CREATE INDEX `cms_engine_banner_page_id_idx`
+ON `cms_engine_banner` (`page_id`);
+
+CREATE INDEX `cms_engine_banner_project_id_idx`
+ON `cms_engine_banner` (`project_id`);
+
+CREATE INDEX `cms_engine_banner_status_vi_idx`
+ON `cms_engine_banner` (`status_vi`);
+
+CREATE INDEX `cms_engine_banner_type_idx`
+ON `cms_engine_banner` (`type`);
+
+CREATE INDEX `cms_engine_banner_sorted_as_idx`
+ON `cms_engine_banner` (`sorted_as`);
