788537f656bbb0abc2d190127a3347d6671ecbd9
Nam Dinh <namdwb@gmail.com>
2026-05-31 13:50:07 +0700

    fix: table name

--- 1.hqt_elevator/sql/migrations/0005_alter_banner_options_alter_marketingclaim_options_and_more.sql
+++ 1.hqt_elevator/sql/migrations/0005_alter_banner_options_alter_marketingclaim_options_and_more.sql
@@ -4,25 +4,25 @@
 -- =========================================
 
 -- 1. RENAME COLUMN
-ALTER TABLE site
+ALTER TABLE cms_engine_site
 RENAME COLUMN extra_robots TO robots_extra;
 
 -- 2. DROP FIELD
-ALTER TABLE marketingclaim
+ALTER TABLE cms_engine_marketingclaim
 DROP COLUMN icon;
 
 -- 3. ADD FIELDS
 
-ALTER TABLE branch
+ALTER TABLE cms_engine_branch
 ADD COLUMN icon_img VARCHAR(255) NULL;
 
-ALTER TABLE highlight
+ALTER TABLE cms_engine_highlight
 ADD COLUMN icon_img VARCHAR(255) NULL;
 
-ALTER TABLE socialnetwork
+ALTER TABLE cms_engine_socialnetwork
 ADD COLUMN icon_img VARCHAR(255) NULL;
 
-ALTER TABLE marketingclaim
+ALTER TABLE cms_engine_marketingclaim
 ADD COLUMN heading_vi VARCHAR(255) NULL,
 ADD COLUMN icon_code VARCHAR(30) NULL,
 ADD COLUMN icon_img VARCHAR(255) NULL,
@@ -32,28 +32,28 @@ ADD COLUMN subheading_vi VARCHAR(255) NULL;
 
 -- 4. ALTER EXISTING FIELDS
 
-ALTER TABLE banner
+ALTER TABLE cms_engine_banner
 MODIFY COLUMN image_vi VARCHAR(255) NULL;
 
-ALTER TABLE landing
+ALTER TABLE cms_engine_landing
 MODIFY COLUMN backgroud_footer VARCHAR(255) NULL,
 MODIFY COLUMN backgroud_header VARCHAR(255) NOT NULL,
 MODIFY COLUMN logo VARCHAR(255) NULL;
 
-ALTER TABLE license
+ALTER TABLE cms_engine_license
 MODIFY COLUMN logo VARCHAR(255) NULL;
 
-ALTER TABLE marketingclaim
+ALTER TABLE cms_engine_marketingclaim
 MODIFY COLUMN is_active TINYINT(1) NULL DEFAULT 1;
 
-ALTER TABLE page
+ALTER TABLE cms_engine_page
 MODIFY COLUMN icon VARCHAR(255) NULL;
 
-ALTER TABLE site
+ALTER TABLE cms_engine_site
 MODIFY COLUMN image404 VARCHAR(255) NULL,
 MODIFY COLUMN imageFavicon VARCHAR(255) NULL,
 MODIFY COLUMN imageLogo VARCHAR(255) NULL,
 MODIFY COLUMN language_type INT NOT NULL DEFAULT 0;
 
 -- 5. DELETE TABLE
-DROP TABLE IF EXISTS TopFeature;
+DROP TABLE IF EXISTS cms_engine_topfeature;
